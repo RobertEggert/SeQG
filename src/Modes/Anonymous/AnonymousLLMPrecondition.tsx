beb077e316118216325c5fb6fb06485b65fb5e06
eggero07 <50808559+RobertEggert@users.noreply.github.com>
2025-05-26 09:40:03 +0200

    code cleaning, useless code removal, fix routing

---
+++ src/Modes/Anonymous/AnonymousLLMPrecondition.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+const AnonymousLLMPrecondition = () => {
+    return (
+        <Box>
+            <h1>
+                Client! Welcome to the Anonymous mode give your age and
+                creditcardnumber!
+            </h1>
+        </Box>
+    );
+};
+
+export default AnonymousLLMPrecondition;
